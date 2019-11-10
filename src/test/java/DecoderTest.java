a02ab44cf3717394958bf4cd76fc3a0b09c86f50
pkminor <minorpk@gmail.com>
2019-11-10 10:21:14 +0300

    updated Decoder decode  method name

--- src/test/java/DecoderTest.java
+++ src/test/java/DecoderTest.java
@@ -19,20 +19,20 @@ public class DecoderTest {
     @Test
     public void decoder_decryptText_DecoderObject(){
         Decoder decode = new Decoder();
-        assertEquals(true,decode.equals(decode.decryptText("Z")) );
+        assertEquals(true,decode.equals(decode.decrypt("Z")) );
     }
 
     @Test
     public void decoder_decryptSingleChar_String(){
         Decoder decode = new Decoder();
         decode.setKey(1);
-        assertEquals("A",decode.decryptText("B").getPlainText() );
+        assertEquals("A",decode.decrypt("B").getPlainText() );
     }
 
     @Test
     public void decoder_decryptManyChar_String(){
         Decoder decode = new Decoder();
         decode.setKey(1);
-        assertEquals("ABC",decode.decryptText("BCD").getPlainText() );
+        assertEquals("ABC",decode.decrypt("BCD").getPlainText() );
     }
 }
